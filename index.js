33b3d33e760f44d262beaa08f024566860099a9b
RUDRAKSHA KUSHWAHA <kushwaharudraksha@gmail.com>
2023-08-20 11:02:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let time;
+setInterval(() => {
+    let mydate= new Date();
+    let hr=mydate.getHours();
+    let sec = mydate.getSeconds();
+    let min = mydate.getMinutes();
+    if (sec<10) {
+        sec='0'+sec ;
+    }
+    let h;
+    if (hr>=12) {
+        h="pm"
+    }
+    else{
+        h="am"
+    }
+    if(min<10){
+        min ="0"+min;
+    }
+    time =  (hr%12)+":"+ min+":"+sec+" "+h;
+    document.getElementById('time').innerHTML=time;
+}, 1000);
+let mydate= new Date();
+arr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+date = mydate.toLocaleDateString();
+document.getElementById('date').innerHTML= date;
+document.getElementById('days').innerHTML=arr[mydate.getDay()];
